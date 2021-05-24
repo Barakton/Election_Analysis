455ffab9744507951a29322fd35d39d8a8e8d9de
Logan Mayes <leadrts@gmail.com>
2021-05-23 22:34:58 -0500

    Adding the election analysis.

---
+++ Python_practice.py
@@ -0,0 +1,15 @@
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+    """candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100:.1f}% of the total votes.")
+
+print(message_to_candidate)"""
